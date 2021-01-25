d85ad77e21f66313788c5266b6cbaa8ac2743356
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 10:23:51 +0100

    Add project files.

---
+++ ValutaOmregner/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValutaOmregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Indsæt DKK som du vil omregne");
+
+            double dkk = double.Parse(Console.ReadLine());
+
+            string usd = $"USD = {dkk * 0.16}";
+
+            string pund = $"pund = {dkk * 0.12}";
+
+            string euro = $"Euro = {dkk * 0.13}";
+
+            string sek = $"SEK = {dkk * 1.36}";
+
+            Console.WriteLine(usd);
+            Console.WriteLine(pund);
+            Console.WriteLine(euro);
+            Console.WriteLine(sek);
+
+            Console.ReadKey();
+        }
+    }
+}
